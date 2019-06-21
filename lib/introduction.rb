12bdc426f1b59dc161cb225434a18b3f86487670
Marco Frisina <marco@frisina.xyz>
2019-06-21 19:21:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,9 @@
 def introduction(name)
-  puts "Great to meet you #{name}!"
+  puts "Great to meet you #{name}!";
 end
+
+def introduction_with_language(name, language)
+  puts "Great to see you are studying #{language}, #{name}";
+end
+
+introduction("Marco")
